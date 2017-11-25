d02b15e52763f80b5fccc1867f0b2aa14f91c3ca
Aleksey Zolochevskiy <aleksey.zolochevski@gmail.com>
2017-11-25 12:01:52 +0200

    + closeModal; + unwrap function

--- modals/modals.js
+++ modals/modals.js
@@ -28,36 +28,49 @@ function create(tagName, classNames) {
     return element;
 }
 
-var elements = document.querySelectorAll("[data-modal]");
 
+function unwrap(target, wrapper) {
+
+    // TODO: insertBefore(target, wrapper)
+    var parent = wrapper.parentNode;
+    parent.insertBefore(target, wrapper);
+
+    // TODO: hide(target)
+    target.style.display = "none";
+
+    remove(wrapper);
+}
+
+var closeModal = function (event) {
+    if (event.target === event.currentTarget) {
+        var target = this.firstElementChild;
+        var backdrop = document.querySelector('.backdrop');
+
+        unwrap(target,this);
+        remove(backdrop);
+    }
+};
+
+// TODO: addEventListenerForAll("[data-dropdown]", 'click', function(event){...})
+var elements = document.querySelectorAll("[data-modal]");
 for (var i = 0; i < elements.length; i++) {
     elements[i].addEventListener('click', function (event) {
 
         var targetId = this.getAttribute('data-modal');
 
         var target = document.getElementById(targetId);
-        target.style.display = "block";
 
         var wrapper = create('div', ['modal-wrapper']);
-        var parent = target.parentNode;
         wrap(target, wrapper);
 
         var backdrop = create('div', ['backdrop']);
 
-        // TODO: create prependChild function
+        // TODO: prependChild(document.body, backdrop)
         document.body.insertBefore(backdrop, document.body.firstChild);
 
-        wrapper.addEventListener('click', function (event) {
-            if (event.target === event.currentTarget) {
-                // TODO: try to less use outside variables
+        // TODO show(target)
+        target.style.display = "block";
 
-                // TODO: create unwrap function
-                parent.insertBefore(target, wrapper);
-                remove(wrapper);
-                remove(backdrop);
-
-                target.style.display = "none";
-            }
-        });
+        wrapper.addEventListener('click', closeModal);
     });
 }
